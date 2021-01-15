26665db2189d2da06d3b92d5f3d0b559156446ec
BakariF <bakkfofhit@live.fr>
2021-01-15 13:59:51 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exercice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bigfoot = 12;
+            int secondBigfoot = 25;
+            int sum = bigfoot + secondBigfoot;
+            Console.WriteLine(bigfoot + secondBigfoot);
+            Console.WriteLine("babayaga" + sum);
+            Console.WriteLine((bigfoot + secondBigfoot) + "babayaga ?")  ;
+        }
+    }
+}
